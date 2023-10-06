2ff48a69fa576f734c87870a0f52bb47d4476dc9
chochy2001 <ohchochy@gmail.com>
2023-10-06 16:17:54 -0600

    primer commit app iampoor

---
+++ iampoor/iampoor/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by Jorge Salgado Miranda on 06/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Agregando un comentario para que se cambie
+        // Do any additional setup after loading the view.
+    }
+}
+
