b2426d26dae75b4a9e8cf481c0661ec19e296fbe
chochy2001 <ohchochy@gmail.com>
2023-10-06 18:05:44 -0600

    Se termino la aplicacion de los dados

--- Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    //Allows me to reference a UI element, in this example is the dice images.
+    
+    
+    @IBAction func buttonC() {
+        let randomIndex1 = Int.random(in: 0..<diceArray.count)
+        let randomImage1 = UIImage(named: diceArray[randomIndex1])
+        let randomIndex2 = Int.random(in: 0..<diceArray.count)
+        let randomImage2 = UIImage(named: diceArray[randomIndex2])
+        diceeImageView1.image = randomImage1
+        diceeImageView2.image = randomImage2
+    }
+    
+    
+    @IBOutlet weak var diceeImageView2: UIImageView!
+    @IBOutlet weak var diceeImageView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceeImageView1.image = UIImage(imageLiteralResourceName: diceOne)
+        diceeImageView2.image = UIImage(imageLiteralResourceName: diceTwo)
+        
     }
-
-
 }
 
---
+++ Dicee-iOS13-master/Dicee-iOS13/constants.swift
@@ -0,0 +1,22 @@
+//
+//  constants.swift
+//  Dicee-iOS13
+//
+//  Created by Jorge Salgado Miranda on 06/10/23.
+//  Copyright © 2023 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+//name of the images of the dicees
+let diceOne = "DiceOne"
+let diceTwo = "DiceTwo"
+let diceThree = "DiceThree"
+let diceFour = "DiceFour"
+let diceFive = "DiceFive"
+let diceSix = "DiceSix"
+
+//array with the name of the dices
+let diceArray = [diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix]
+
+
